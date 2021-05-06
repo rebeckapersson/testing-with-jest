f593b51085478f08e421fd1314be359266934a81
rebeckapersson <rebecka_fotboll95@hotmail.com>
2021-05-06 09:53:03 +0200

    gulmarkerad

---
+++ src/stack.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+
+let stack = [];
+
+//lägger ett element överst i stacken
+exports.push = function (x){
+  stack.push(x);
+};
+
+//returnerar det översta elementet i stacken och tar bort det
+exports.pop = function (){
+  return stack.pop();
+};
+
+//returnerar det översta elementet i stacken
+exports.peek = function (){
+  return stack[0]; //det här är medvertet felaktigt
+}; 
